0d83cadd32e2edd96f23e76949ff3de2385dc69a
arun4006 <aarunkumar334@gmail.com>
2023-06-29 20:25:30 +0530

    added

---
+++ Azure_oauth/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Azure_oauth.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
